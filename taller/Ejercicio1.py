97921529d5efc282850a1c91004a8b414691dd4d
Sebastian <saguileran@unal.edu.co>
2018-09-08 17:54:19 -0500

    first finish

--- taller/Ejercicio1.py
+++ taller/Ejercicio1.py
@@ -1,26 +1,34 @@
 from turtle import *
 import  math
-#turtle.forward(100)
+#speed(1)
+a,b=-1,1
 def cuadrado(l,c):
     ini=position()
     penup();pensize(4)
-    setpos(ini+(-c/2,-c/2))
+    setpos(ini+(a*c/2,-b*c/2))
     pendown()
     for i in range(l):
         forward(c)
         left(360/l)
     penup()
-    setpos(ini)
+    goto(ini)
     pendown()
-hideturtle()
-#cuadrado(4,100)
+    pensize(1)
+    heading()
+#hideturtle()
+#cuadrado(4,10)
+penup()
+setpos(position()+(-100,-100))
 for j in range(4):
-  # penup()
-   ini1=position()
-   setpos(ini1+(-500/2,-500/2))
+   penup()
+  # ini1=position()
+   #setpos(ini1+(-200/2,-200/2))
   # pendown()
-   forward(100)
-  # cuadrado(4,100)
+   forward(200)
+   cuadrado(4,100)
    left(90)
    #setpos(ini1)
-   break
+   if j%2==0:
+     a=-a
+   else:
+     b=-b
