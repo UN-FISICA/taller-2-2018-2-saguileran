1ba4f03fb9ba9be54fecdbcd2a2f8cf33bfb1924
Sebastian <saguileran@unal.edu.co>
2018-09-09 12:41:47 -0500

    Files corrected

--- taller/Ejercicio3.py
+++ taller/Ejercicio3.py
@@ -3,10 +3,10 @@ import  math
 def figura(l):
     ini=position()
     penup()
-    goto(ini+(-50/2,-50/2))
+    goto(ini+(-25/2,-25/2))
     pendown()
     for i in range(l):
-        forward(50)
+        forward(25)
         left(360/l)
     penup()
     goto(ini)
@@ -15,13 +15,14 @@ pensize(4)
 a,b,l=-1,1,int(input('Enter number of sides: '))
 L=int(input('Enter number of sides big: '))
 penup()
-goto(-100,-100)
+goto(-200,-200)
 for j in range(L):
-	forward(200)
+	forward(100)
 	left(90)
 	seth(0)
 	figura(l)
 	seth(0)
 	left((360/L)+(360/L)*j)
 pendown()
+
 
