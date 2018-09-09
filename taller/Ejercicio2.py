bbc3600b943847f2d84ff28549ed8cd722c41ab2
Sebastian <saguileran@unal.edu.co>
2018-09-09 11:42:42 -0500

    first exercise improve and second finish

--- taller/Ejercicio2.py
+++ taller/Ejercicio2.py
@@ -1,26 +1,26 @@
 from turtle import *
 import  math
-def cuadrado(l):
+def figura(l):
     ini=position()
     penup()
-    goto(ini+(-100/2,-100/2))
+    goto(ini+(-50/2,-50/2))
     pendown()
-    for i in range(4):
-        forward(100)
+    for i in range(l):
+        forward(50)
         left(360/l)
     penup()
     goto(ini)
-speed(10)
 hideturtle()
 pensize(4)
-a,b=-1,1
+a,b,l=-1,1,int(input('Enter number of sides: '))
 penup()
-goto(-200,-200)
+goto(-100,-100)
 for j in range(4):
-	forward(400)
+	forward(200)
 	left(90)
 	seth(0)
-	cuadrado(4)
-	heading()
-	left(90+90*j)
+	figura(l)
+	seth(0)
+	left(90+(90)*j)
 pendown()
+
