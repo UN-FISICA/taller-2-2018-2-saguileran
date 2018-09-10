cf784c60236fa7897f29334427f5e8f56f379c3a
saguileran <42812846+saguileran@users.noreply.github.com>
2018-09-09 23:35:39 -0500

    Update Ejercicio6.py

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -28,16 +28,19 @@ pensize(4)
 penup()
 r=40
 H = 2*r*math.sin(math.pi/l)
-
+penup()
+for i in range(3):
+    forward(lon)
+    left(120)
+    if i==1: Pos=position()
+pendown()
 for j in range(f):
     if j%2==0:seth(0)
     else:seth(180)
     for k in range(f-j-1):
         figura(l-j)
         if f-(j+1)!=0: 
-            forward((c/f*(f-j))/(f-(j+1)))
-            print((c/f*(f-j))/(f+(j+1)),j)
-            #forward(((c/f*(f-j))/(f-(j+1))-3**(j+1)))
+            forward((c/f*(f-j))/(f-(j+1))) 
         if k==f-j-2: figura(l-j)
     if j!=f-1:
         if j%2==0:left(120)
