e4375f49a1171b63fade8b68c95b7dd05243d140
saguileran <42812846+saguileran@users.noreply.github.com>
2018-09-09 23:28:45 -0500

    Update Ejercicio6.py

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep  8 18:27:05 2018
-
-@author: sebas
-"""
-
-def figura(l):
+# -*- coding: utf-8 -*-def figura(l):
     Hini=heading()
     ini=position()
     seth(0)
@@ -14,7 +7,7 @@ def figura(l):
     goto(ini+(-40/2,-40/2))
     pendown()
     for i in range(l):
-        forward(40) 
+        forward(H) 
         left(360/l)
     penup()
     goto(ini)
@@ -37,21 +30,19 @@ for i in range(3):
     if i==1: Pos=position()
 pensize(4)
 penup()
-r=5
+r=40
 H = 2*r*math.sin(math.pi/l)
+
 for j in range(f):
     if j%2==0:seth(0)
     else:seth(180)
     for k in range(f-j-1):
-        figura(int(H-j+2))
+        figura(l-j)
         if f-(j+1)!=0: 
-            if j==f-2:
-                forward((c/f*(f-j))/(f-(j+1))-(j)*(c/10))
-            else:
-                forward((c/f*(f-j))/(f-(j+1))-(j)*(c/20))
-            #print((c/f*(f-j))/(f+(j+1)),j)
+            forward((c/f*(f-j))/(f-(j+1)))
+            print((c/f*(f-j))/(f+(j+1)),j)
             #forward(((c/f*(f-j))/(f-(j+1))-3**(j+1)))
-        if k==f-j-2: figura(int(H-j+2))
+        if k==f-j-2: figura(l-j)
     if j!=f-1:
         if j%2==0:left(120)
         else:right(120)  
@@ -64,4 +55,5 @@ for j in range(f):
         #forward(((c/f*(f-j))/(f-(j+2)))/2-30*(k+1))
         if j==f-1: 
             goto(Pos)
-            figura(int(H-j+2))
+            figura(l-j)
+     
